e611238b0c2cceb7c13d3359c1dbbeebbe298965
Edd Purcell <epurcell3@gatech.edu>
2012-10-29 21:27:23 -0400

    Included expanded view into the search.
    Needs a lot of styling and cannot be gotten rid of yet. To be included.

--- examples/unified_search/unifiedsearch.js
+++ examples/unified_search/unifiedsearch.js
@@ -19,8 +19,8 @@ function BaseSearch(name){
 twitter_search = new BaseSearch("Twitter Search");
 twitter_search.search = function(internalpage, keyword){
     if(internalpage === undefined || internalpage < 1) internalpage = 1;
-    var url = "http://search.twitter.com/search.json?callback=twitter_search.handler&rpp=" + 
-        50 + "&page=" + internalpage + "&geocode=" + myLat + "%2C" + myLong + 
+    var url = "http://search.twitter.com/search.json?callback=twitter_search.handler&rpp=" +
+        50 + "&page=" + internalpage + "&geocode=" + myLat + "%2C" + myLong +
         "%2C" + radius;
     if(keyword.charAt(0) == '#') url += "&tag=" + keyword.substring(1,keyword.length);
     else if(keyword != '') url += "&q=" + keyword;
@@ -36,13 +36,19 @@ twitter_search.handler = function(data){
     var innerhtml = "";
     for(var i = 0, length = data.results.length; i < length; i++){
         if(data.results[i].geo != null){
-            innerhtml += "<div class=twitsearch id =\"" + searchdata.length + 
-                "\"><img style=\"float:left\" src=" + twiticon + "/>\n" + 
+            innerhtml += "<div class=twitsearch id=Twtt" + i +
+                "\"><img style=\"float:left\" src=" + twiticon + "/>\n" +
                 "<span class=twitusername>" + data.results[i].from_username + "</span><br/>\n" +
-                "<span class=twittext>" + data.results[i].text + "</span></div>\n";
+                "<span class=twittext>" + this.slugify(data.results[i].text) + "</span></div>\n";
+            localStorage.setItem("unified_search_" + "Twitt_" + i, JSON.stringify(data.results[i]));
         }
     }
     document.getElementById("view").innerHTML += innerhtml;
+}
+twitter_search.expand = function(){
+}
+twitter_search.slugify = function(str){
+    return str.substring(20) + "...";
 }
 searches[0] = twitter_search;
 add_to_search_select(0);
@@ -50,9 +56,13 @@ add_to_search_select(0);
 flickr_search = new BaseSearch("Flickr Search");
 flickr_search.search = function(internalpage, keyword){
     if(internalpage === undefined || internalpage < 1) internalpage = 1;
-    var url = "http://api.flickr.com/services/rest/?method=flickr.photos.search" + 
-        "&api_key=7e893653fffba0face9ae992aa2fdf12" + 
-        "&tags=" + keyword + "&per_page=20&page=0&format=json" + "&jsoncallback=flickr_search.handler" + 
+    if(keyword.charAt(0) == '#')
+    {
+        keyword = keyword.substring(1);
+    }
+    var url = "http://api.flickr.com/services/rest/?method=flickr.photos.search" +
+        "&api_key=7e893653fffba0face9ae992aa2fdf12" +
+        "&tags=" + keyword + "&per_page=20&page=0&format=json" + "&jsoncallback=flickr_search.handler" +
         "&has_geo=true&lat=" + myLat + "&lon=" + myLong + "&radius=" + radius + "&extras=owner_name,geo," +
         "url_sq,url_m";
     scriptElement = document.createElement("SCRIPT");
@@ -62,25 +72,30 @@ flickr_search.search = function(internalpage, keyword){
     document.getElementsByTagName('head')[0].appendChild(scriptElement);
 }
 flickr_search.handler = function(data){
-   scriptElement = document.getElementById('injected');
-   scriptElement.parentNode.removeChild(scriptElement);
-   var innerhtml = '';
-   for(var i = 0, length = data.photos.photo.length; i < length; i++){
-       innerhtml += "<div class=flickrsearch id =\"" + searchdata.length + "\"><img src=\"" + 
+    scriptElement = document.getElementById('injected');
+    scriptElement.parentNode.removeChild(scriptElement);
+    var innerhtml = '';
+    for(var i = 0, length = data.photos.photo.length; i < length; i++){
+        innerhtml += "<div class=flickrsearch id =\"" + i + "\" onclick=\"javascript:" +
+           "flickr_search.expand(" + i + ")\"><img src=\"" +
            data.photos.photo[i].url_sq + "\"/></div>";
+        localStorage.setItem("unified_search_" + "Flick_" + i, JSON.stringify(data.photos.photo[i]));
    }
    document.getElementById("view").innerHTML += innerhtml;
+}
+flickr_search.expand = function(index){
+    var data = JSON.parse(localStorage["unified_search_" + "Flick_" + index]);
+    var wrapper = document.querySelector('.measuring-wrapper');
+    wrapper.innerHTML = "<div class=flickrexpand><img src=\"" + data.url_m + "\"/>" +
+        "<p>Title: " + data.title + "</p><p>Author: " + data.ownername + "</p></div>";
+    grow();
 }
 searches[1] = flickr_search;
 add_to_search_select(1);
 
-//Find better way to do this. Fuck you Flickr.
-function jsonFlickrApi(data){
-    flickr_search.handler(data);
-}
-
 function runsearch(keyword){
     document.getElementById('view').innerHTML = "";
+    clear_data();
     for(var i = 0, length = searches.length; i < length; i++){
         if(searches[i].activated) searches[i].search(pagenumber, keyword);
     }
@@ -113,4 +128,23 @@ function next_page(){
 
 function prev_page(){
     pagenumber--;
+}
+
+function clear_data(){
+    for(key in localStorage){
+        if(key.indexOf("unified_search_") != -1){
+            localStorage.removeItem(key);
+        }
+    }
+}
+
+function grow(){
+    var expand = document.getElementById('expand');
+    if(expand.clientHeight) {
+        expand.style.height = 0;
+    }
+    else {
+    var wrapper = document.querySelector('.measuring-wrapper');
+        expand.style.height = wrapper.clientHeight + "px";
+    }
 }
