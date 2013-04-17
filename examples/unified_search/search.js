d0673554c30666860c5742558e20b04428348b31
Edd <edd.purcell@gmail.com>
2013-04-17 02:39:37 -0400

    GeoObjects aren't playing nice. Workable version using CSSObjects and setting the position based on lla info is set up for now.

--- examples/unified_search/search.js
+++ examples/unified_search/search.js
@@ -22,15 +22,30 @@ function format(bundle){
         JSLOG.log("Formatting to AR mode.");
         //JSLOG.log(item.body);
         var div = document.createElement("div");
-        div.style.width = "300px";
-        div.style.height = "110px";
+        div.style.width = "3000px";
+        div.style.height = "1100px";
         div.innerHTML = item.body;
         var cssObj = new THREE.CSSObject(div);
-        cssObj.width = 300;
-        cssObj.height = 110;
-        cssObj.position.z = -1000;
+        cssObj.width = 3000;
+        cssObj.height = 1100;
         
-        //var geoObj = new ARGON.createGeoObject(item.lat, item.long, 0);
+        var devloc = ARGON.geolocation.getLLA();
+        var vector = {lat:item.lat - devloc.latitude,
+                      long:item.long - devloc.longitude, 
+                      dist:0};
+        vector.dist = Math.pow((Math.pow(vector.lat,2) + Math.pow(vector.long,2)), 0.5);
+        
+        //Put everything at a radius of 3500 for now
+        var objDist = 3500;
+        cssObj.position.z = objDist * vector.lat/vector.dist;
+        cssObj.position.x = objDist * vector.long/vector.dist;
+        cssObj.position.y = -400; //too high up?
+        //JSLOG.log("(" + cssObj.position.z + "," + cssObj.position.y + ")");
+        
+        
+        // Can't get GeoObjects to work nicely right now
+        // Handling pure CSSObjects will be easier for now
+        //var geoObj = ARGON.createGeoObject(item.lat, item.long, 0);
         //geoObj.add(cssObj);
         
         ARGON.World.add(cssObj);
